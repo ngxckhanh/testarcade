612e853542a3a882daf4dc25e71be0242ac7209c
meomeowawa <ngxckhanh@gmail.com>
2025-11-22 19:59:18 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,8 @@
 const btn = document.getElementById("playBtn");
-const lockedMsg = document.getElementById("lockedMessage");
 
-// Check if arcade has already been played
-if (localStorage.getItem("saola_arcade_played") === "yes") {
-    btn.innerText = "Play Again";
-    btn.disabled = true; // disable button
-    lockedMsg.innerText = "You can only play the entire arcade once!";
-    lockedMsg.style.color = "red";
-    lockedMsg.style.fontWeight = "bold";
-} else {
-    // First-time player → start Sorting Game
     btn.addEventListener("click", () => {
         window.location.href = "info.html";
     });
 }
+
 
