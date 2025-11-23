f84ad18439f76724d94d79b4086cca3531cf78d8
meomeowawa <ngxckhanh@gmail.com>
2025-11-23 23:42:44 +0700

    Update final.js

--- js/final.js
+++ js/final.js
@@ -1,4 +1,19 @@
-// Load total score
+// ---- LOAD SCORE ----
 const finalScoreEl = document.getElementById('finalScore');
 let score = parseInt(localStorage.getItem('saola_arcade_score') || '0', 10);
 finalScoreEl.innerText = score;
+
+
+// ---- LOAD PLAYER INFO ----
+const nameEl = document.getElementById('finalName');
+const classEl = document.getElementById('finalClass');
+const emailEl = document.getElementById('finalEmail');
+
+// Read from localStorage
+const playerName = localStorage.getItem('player_name') || 'Không có';
+const playerClass = localStorage.getItem('player_class') || 'Không có';
+const playerEmail = localStorage.getItem('player_email') || 'Không có';
+
+nameEl.innerText = playerName;
+classEl.innerText = playerClass;
+emailEl.innerText = playerEmail;
